ce56ef09dcbf21d16ff472140c48ead110add831
Andre-Gross <andre.handy94@gmail.com>
2025-04-15 11:36:23 +0200

    Added all Endboss images

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -1,7 +1,7 @@
 class Endboss extends MovableObject {
     originalImgHeight = 1217;
     originalImgWidth = 1045;
-    
+
     height = this.calculateHeight();
     width = this.calculateWidth();
 
@@ -27,11 +27,42 @@ class Endboss extends MovableObject {
         'assets/img/4_enemie_boss_chicken/2_alert/G11.png',
         'assets/img/4_enemie_boss_chicken/2_alert/G12.png',
     ]
+    IMAGES_ATTACK = [
+        './assets/img/4_enemie_boss_chicken/3_attack/G13.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G14.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G15.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G16.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G17.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G18.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G19.png',
+        './assets/img/4_enemie_boss_chicken/3_attack/G20.png',
+    ]
+    IMAGES_DEAD = [
+        './assets/img/4_enemie_boss_chicken/5_dead/G24.png',
+        './assets/img/4_enemie_boss_chicken/5_dead/G25.png',
+        './assets/img/4_enemie_boss_chicken/5_dead/G26.png',
+    ]
+    IMAGES_HURT = [
+        './assets/img/4_enemie_boss_chicken/4_hurt/G21.png',
+        './assets/img/4_enemie_boss_chicken/4_hurt/G22.png',
+        './assets/img/4_enemie_boss_chicken/4_hurt/G23.png',
+    ]
+    IMAGES_WALK = [
+        './assets/img/4_enemie_boss_chicken/1_walk/G1.png',
+        './assets/img/4_enemie_boss_chicken/1_walk/G2.png',
+        './assets/img/4_enemie_boss_chicken/1_walk/G3.png',
+        './assets/img/4_enemie_boss_chicken/1_walk/G4.png',
+    ]
 
     constructor() {
         super().loadImage('assets/img/4_enemie_boss_chicken/2_alert/G5.png');
         this.loadImages(this.IMAGES_ALERT);
         this.x = 2700 / backgroundImgOriginalHeight * canvasHeight;
+        this.loadImages(this.IMAGES_ATTACK);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_WALK);
+        this.x = 600 / backgroundImgOriginalHeight * canvasHeight;
         this.y = this.calculateY();
 
         this.animate()
