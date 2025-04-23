5ca5d6e13b3b0cc457b165ba640ff2434a4bc996
Andre-Gross <andre.handy94@gmail.com>
2025-04-23 13:53:49 +0200

    added function to start the game

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -22,7 +22,7 @@ class Chicken extends MovableObject {
     maxAdditionalSpeedXPerSecond = 18;
 
     standartSpeedXPerFrame = this.calculateSpeedPerFrame();
-    speedXPerFrame = this.standartSpeedXPerFrame;
+    speedXPerFrame;
 
     IMAGES_WALK = [
         './assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
--- js/game.js
+++ js/game.js
@@ -4,11 +4,68 @@ let keyboard = new Keyboard();
 
 let canvasHeight = 480;
 let canvasWidth = 1920 * 480 / 1080;
-const backgroundImgOriginalHeight  = 1080;
+const backgroundImgOriginalHeight = 1080;
 let backgroundHeightFactor = canvasHeight / backgroundImgOriginalHeight;
 
 
 let maxFPS = 60;
+
+
+function activateKeyboard() {
+    activateKeyDown();
+    activateKeyUp();
+}
+
+
+function activateKeyDown() {
+    document.addEventListener('keydown', (event) => {
+        let key = event.key;
+
+        if (key === 'd') {
+            keyboard.D = true;
+        } else if (key === 'ArrowRight') {
+            keyboard.ARROW_RIGHT = true;
+        } else if (key === 'a') {
+            keyboard.A = true;
+        } else if (key === 'ArrowLeft') {
+            keyboard.ARROW_LEFT = true;
+        } else if (key === 'w') {
+            keyboard.W = true;
+        } else if (key === ' ') {
+            keyboard.SPACE = true;
+        } else if (key === 'ArrowUp') {
+            keyboard.ARROW_UP = true;
+        } else if (key === 't') {
+            keyboard.T = true
+        }
+    })
+}
+
+
+function activateKeyUp() {
+    document.addEventListener('keyup', (event => {
+        let key = event.key;
+
+        if (key === 'd') {
+            keyboard.D = false;
+        } else if (key === 'ArrowRight') {
+            keyboard.ARROW_RIGHT = false;
+        } else if (key === 'a') {
+            keyboard.A = false;
+        } else if (key === 'ArrowLeft') {
+            keyboard.ARROW_LEFT = false;
+        } else if (key === 'w') {
+            keyboard.W = false;
+        } else if (key === ' ') {
+            keyboard.SPACE = false;
+        } else if (key === 'ArrowUp') {
+            keyboard.ARROW_UP = false;
+        } else if (key === 't') {
+            keyboard.T = false;
+        }
+    }))
+}
+
 
 function init() {
     canvas = document.getElementById('canvas');
@@ -18,47 +75,12 @@ function init() {
 }
 
 
-document.addEventListener('keydown', (event) => {
-    let key = event.key;
-
-    if (key === 'd') {
-        keyboard.D = true;
-    } else if (key === 'ArrowRight') {
-        keyboard.ARROW_RIGHT = true;
-    } else if (key === 'a') {
-        keyboard.A = true;
-    } else if (key === 'ArrowLeft') {
-        keyboard.ARROW_LEFT = true;
-    } else if (key === 'w') {
-        keyboard.W = true;
-    } else if (key === ' ') {
-        keyboard.SPACE = true;
-    } else if (key === 'ArrowUp') {
-        keyboard.ARROW_UP = true;
-    } else if (key === 't') {
-        keyboard.T = true
-    }
-})
+function startGame() {
+    activateKeyboard();
+    world.isGameStarted = true;
+    world.level.enemies.forEach(enemy => {
+        enemy.speedXPerFrame = this.standartSpeedXPerFrame
+    });
+}
 
 
-document.addEventListener('keyup', (event => {
-    let key = event.key;
-
-    if (key === 'd') {
-        keyboard.D = false;
-    } else if (key === 'ArrowRight') {
-        keyboard.ARROW_RIGHT = false;
-    } else if (key === 'a') {
-        keyboard.A = false;
-    } else if (key === 'ArrowLeft') {
-        keyboard.ARROW_LEFT = false;
-    } else if (key === 'w') {
-        keyboard.W = false;
-    } else if (key === ' ') {
-        keyboard.SPACE = false;
-    } else if (key === 'ArrowUp') {
-        keyboard.ARROW_UP = false;
-    } else if (key === 't') {
-        keyboard.T = false;
-    }
-}))
