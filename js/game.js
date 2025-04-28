2488b9bdd2b9ec707bebf87cf808bcf4a9afe291
Andre-Gross <andre.handy94@gmail.com>
2025-04-28 13:44:25 +0200

    added toogleDisplayNone()

--- js/game.js
+++ js/game.js
@@ -109,10 +109,35 @@ function toggleControls() {
         toggleDisplayNone(canvas, 'd-flex', false);
     }
 }
+
+
+/**
+ * Toggles the display of an element based on the specified mode.
+ * @param {HTMLElement} element - The DOM element to toggle.
+ * @param {string} displayMode - The CSS class to apply when visible (default: "d-block").
+ * @param {boolean|string} shallVisible - Whether the element should be visible or hidden, or toggle state if undefined.
+ */
+function toggleDisplayNone(
+    element,
+    displayMode = "d-block",
+    shallVisible = ""
+) {
+    let eleClass = element.classList;
+    if (shallVisible === true) {
+        eleClass.remove("d-none");
+        eleClass.add(displayMode);
+    } else if (shallVisible === false) {
+        eleClass.remove(displayMode);
+        eleClass.add("d-none");
+    } else {
+        if (eleClass.contains("d-none")) {
+            eleClass.remove("d-none");
+            eleClass.add(displayMode);
         } else {
-            enemy.standartSpeedXPerFrame = enemy.calculateSpeedPerFrame();
+            eleClass.remove(displayMode);
+            eleClass.add("d-none");
         }
-    });
+    }
 }
 
 
