67c034a86a4f4b035c428a5f74361beb9652b046
Andre-Gross <andre.handy94@gmail.com>
2025-04-29 14:14:40 +0200

    added script.js

--- js/game.js
+++ js/game.js
@@ -109,34 +109,6 @@ function toggleControls() {
         toggleDisplayNone(canvas, 'd-flex', false);
     }
 }
-
-
-/**
- * Toggles the display of an element based on the specified mode.
- * @param {HTMLElement} element - The DOM element to toggle.
- * @param {string} displayMode - The CSS class to apply when visible (default: "d-block").
- * @param {boolean|string} shallVisible - Whether the element should be visible or hidden, or toggle state if undefined.
- */
-function toggleDisplayNone(
-    element,
-    displayMode = "d-block",
-    shallVisible = ""
-) {
-    let eleClass = element.classList;
-    if (shallVisible === true) {
-        eleClass.remove("d-none");
-        eleClass.add(displayMode);
-    } else if (shallVisible === false) {
-        eleClass.remove(displayMode);
-        eleClass.add("d-none");
-    } else {
-        if (eleClass.contains("d-none")) {
-            eleClass.remove("d-none");
-            eleClass.add(displayMode);
-        } else {
-            eleClass.remove(displayMode);
-            eleClass.add("d-none");
-        }
         toggleDisplayNone(startGameButton, 'd-inline-block', false);
         toggleDisplayNone(showControlsButton, 'd-inline-block', false);
         toggleDisplayNone(showStartScreenButton, 'd-inline-block', false)
