8f85932b93ef03d4f8629c72ca3b209a340a2239
Andre-Gross <andre.handy94@gmail.com>
2025-06-11 19:56:39 +0200

    moved some code from LostOverlay to OverlayObject

--- classes/lost-overlay.class.js
+++ classes/lost-overlay.class.js
@@ -30,26 +30,6 @@ class LostOverlay extends OverlayObject {
 
     constructor() {
         super()
-        const imageData = this.choosePicture();
-        
-        this.loadImage(imageData.imagePath);
-        this.originalImgHeight = imageData.originalHeight;
-        this.sizeFactor = imageData.sizeFactor;
-        this.height = this.calculateHeight(imageData.originalWidth);
-        this.width = this.calculateWidth(imageData.originalWidth);
-
-        this.x = (canvasWidth - this.width) / 2;
-        this.y = (canvasHeight - this.height) / 2;
-    }
-
-
-    choosePicture() {
-        let num = Math.random() * 4;
-        num = Math.round(num);
-        if (num === 4) {
-            num = 0;
-        }
-
-        return this.imageDatas[num];
+        this.init();
     }
 }
--- classes/overlay-object.class.js
+++ classes/overlay-object.class.js
@@ -1,5 +1,29 @@
 class OverlayObject extends DrawableObject {
+    sizeFactor = 2;
 
-    sizeFactor = 2
+
+    init() {
+        const imageData = this.choosePicture();
+
+        this.loadImage(imageData.imagePath);
+        this.originalImgHeight = imageData.originalHeight;
+        this.sizeFactor = imageData.sizeFactor;
+        this.height = this.calculateHeight(imageData.originalWidth);
+        this.width = this.calculateWidth(imageData.originalWidth);
+
+        this.x = (canvasWidth - this.width) / 2;
+        this.y = (canvasHeight - this.height) / 2;
+    }
+    
+
+    choosePicture() {
+        let num = Math.random() * 4;
+        num = Math.round(num);
+        if (num === 4) {
+            num = 0;
+        }
+
+        return this.imageDatas[num];
+    }
 
 }
