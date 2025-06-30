f28ea76f9dee68c119b7faa4f30796277b838a6c
Andre-Gross <andre.handy94@gmail.com>
2025-06-30 18:25:41 +0200

    if restarting game after game over, game change to right button set

--- classes/world.class.js
+++ classes/world.class.js
@@ -374,6 +374,7 @@ class World {
         world.isGameFinished = false;
         world.isGameWon = false;
         this.movableStatusbar.setPositionInterval()
+        toggleButtonContainer(true);
     }
 
 
