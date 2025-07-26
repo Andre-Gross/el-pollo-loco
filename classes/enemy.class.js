4b31c1d997cd960e8dfbdcd0eced5a0d8eb587ac
Andre-Gross <andre.handy94@gmail.com>
2025-07-26 11:23:48 +0200

    refactor: clean up Enemy class
    - Replaced magic number 400 with class property 'minSpawnX'
    - Added consistent JSDoc comments for 'init' and 'setSpeedX'
    - Ensured proper spacing between methods

--- classes/enemy.class.js
+++ classes/enemy.class.js
@@ -1,5 +1,12 @@
 class Enemy extends MovableObject {
-    
+    minSpawnX = 400;
+
+
+    /**
+     * Initializes or resets the enemy's health and spawn position.
+     * @param {number} [endOfX=this.endOfX] - The maximum X-coordinate for spawning.
+     * @returns {void}
+     */
     init(endOfX = this.endOfX) {
         this.health = this.standardHealth;
         this.x = this.randomizeSpawnX(endOfX, this.minSpawnX);
@@ -7,9 +14,9 @@ class Enemy extends MovableObject {
 
 
     /**
-     * Sets the horizontal movement speed per frame.
-     * Initializes standard and current speed.
-     * @returns void
+     * Sets the horizontal movement speed per frame based on speed per second.
+     * @param {number} [speedXPerSecond=this.speedXPerSecond] - Speed in pixels per second.
+     * @returns {void}
      */
     setSpeedX(speedXPerSecond = this.speedXPerSecond) {
         this.standardSpeedXPerFrame = this.calculateSpeedPerFrame(speedXPerSecond);
