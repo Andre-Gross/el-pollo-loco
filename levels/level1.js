7056332acd5a971abba0eadf1e27072e15c7956e
Andre-Gross <andre.handy94@gmail.com>
2025-07-29 19:41:32 +0200

    refactor: clarify level1 setup with constants and reduce repetition
    - Introduced constants for level width, background repeat offset, and level end position
    - Replaced repetitive Coin and ThrowableObject instantiations with array mapping
    - Improved code structure for better readability without changing functionality

--- levels/level1.js
+++ levels/level1.js
@@ -1,47 +1,44 @@
-const endOfXInLevel1 = (3840 * backgroundHeightFactor) + 1;
+const BASE_LEVEL_WIDTH = 3840;
+const BACKGROUND_REPEAT_X = 1704;
+const END_OF_LEVEL_X = (BASE_LEVEL_WIDTH * backgroundHeightFactor) + 1;
 
 const level1 = new Level(
-    endOfXInLevel1,
+    END_OF_LEVEL_X,
+
     [
-        new Chicken(endOfXInLevel1),
-        new Chicken(endOfXInLevel1),
-        new Chicken(endOfXInLevel1),
-        new SmallChicken(endOfXInLevel1),
-        new SmallChicken(endOfXInLevel1),
-        new SmallChicken(endOfXInLevel1),
+        new Chicken(END_OF_LEVEL_X),
+        new Chicken(END_OF_LEVEL_X),
+        new Chicken(END_OF_LEVEL_X),
+        new SmallChicken(END_OF_LEVEL_X),
+        new SmallChicken(END_OF_LEVEL_X),
+        new SmallChicken(END_OF_LEVEL_X),
         new Endboss(),
     ],
+
     [
         new BackgroundObject('./assets/img/5_background/layers/air.png', -1),
         new BackgroundObject('./assets/img/5_background/layers/3_third_layer/full.png', -1),
         new BackgroundObject('./assets/img/5_background/layers/2_second_layer/full.png', -1),
         new BackgroundObject('./assets/img/5_background/layers/1_first_layer/full.png', -1),
+
         new BackgroundObject('./assets/img/5_background/layers/air.png'),
         new BackgroundObject('./assets/img/5_background/layers/3_third_layer/full.png'),
         new BackgroundObject('./assets/img/5_background/layers/2_second_layer/full.png'),
         new BackgroundObject('./assets/img/5_background/layers/1_first_layer/full.png'),
-        new BackgroundObject('./assets/img/5_background/layers/air.png', 1, 1704),
-        new BackgroundObject('./assets/img/5_background/layers/3_third_layer/full.png', 1, 1704),
-        new BackgroundObject('./assets/img/5_background/layers/2_second_layer/full.png', 1, 1704),
-        new BackgroundObject('./assets/img/5_background/layers/1_first_layer/full.png', 1, 1704),
+
+        new BackgroundObject('./assets/img/5_background/layers/air.png', 1, BACKGROUND_REPEAT_X),
+        new BackgroundObject('./assets/img/5_background/layers/3_third_layer/full.png', 1, BACKGROUND_REPEAT_X),
+        new BackgroundObject('./assets/img/5_background/layers/2_second_layer/full.png', 1, BACKGROUND_REPEAT_X),
+        new BackgroundObject('./assets/img/5_background/layers/1_first_layer/full.png', 1, BACKGROUND_REPEAT_X),
     ],
+
     [
-        new Cloud(-(3840 + 1)),
+        new Cloud(-(BASE_LEVEL_WIDTH + 1)),
         new Cloud(),
-        new Cloud(3840 - 2),
+        new Cloud(BASE_LEVEL_WIDTH - 2),
     ],
-    [
-        new Coin(endOfXInLevel1),
-        new Coin(endOfXInLevel1),
-        new Coin(endOfXInLevel1),
-        new Coin(endOfXInLevel1),
-        new Coin(endOfXInLevel1)
-    ],
-    [
-        new ThrowableObject(endOfXInLevel1),
-        new ThrowableObject(endOfXInLevel1),
-        new ThrowableObject(endOfXInLevel1),
-        new ThrowableObject(endOfXInLevel1),
-        new ThrowableObject(endOfXInLevel1),
-    ],
-)
+
+    Array(5).fill(null).map(() => new Coin(END_OF_LEVEL_X)),
+
+    Array(5).fill(null).map(() => new ThrowableObject(END_OF_LEVEL_X)),
+);
